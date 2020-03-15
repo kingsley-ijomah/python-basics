79f6a525be1234b5951ddfcc34c32a9704db98bd
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-03-15 11:36:32 +0000

    adding data types

---
+++ 02-data-types.py
@@ -0,0 +1,67 @@
+"""
+String
+Numbers
+Range
+Booleans
+Bytes
+Bytearry
+Memoryview
+Lists
+Tuples
+Sets
+Dictionary
+Frozenset
+
+https://www.w3schools.com/python/python_datatypes.asp
+
+learn at least 3 methods of each data type above where possible e.g on string learn:
+	capitalize()
+	count()
+	join()
+
+https://www.w3schools.com/python/python_strings.asp
+https://www.w3schools.com/python/python_numbers.asp
+https://www.w3schools.com/python/ref_func_range.asp
+https://www.w3schools.com/python/python_booleans.asp
+https://www.w3schools.com/python/ref_func_bytes.asp
+https://www.w3schools.com/python/ref_func_bytearray.asp
+https://www.w3schools.com/python/ref_func_memoryview.asp
+https://www.w3schools.com/python/python_lists.asp
+https://www.w3schools.com/python/python_tuples.asp
+https://www.w3schools.com/python/python_sets.asp
+https://www.w3schools.com/python/python_dictionaries.asp
+https://www.w3schools.com/python/ref_func_frozenset.asp
+
+"""
+
+# test what you learnt
+# --------------------
+'''
+1) what are the four data collection types?
+
+2) create examples of each data type
+
+3) what is the difference between a list and a tuple?
+
+4) is a tuple changeable?
+
+5) is a set data structure indexed?
+
+6) what method would you use to freeze a set?
+
+7) from the below what is the index of "Ford"?
+
+8) thisdict = {
+	  "brand": "Ford",
+	  "model": "Mustang",
+	  "year": 1964
+	}
+
+9) create a set with values "apple", "banana", "cherry" and use the add() function
+   to add "mango" into the set.
+
+10) what would this print out: print(10 > 9) 
+
+11) what would bool('world') print out?
+
+12) what would bool('') print out?
--- README.md
+++ README.md
@@ -3,13 +3,15 @@
 ## Resources
 Python Documentation -- [https://docs.python.org/3.8/tutorial/index.html](https://docs.python.org/3.8/tutorial/index.html)</br>
 Geeks for Geeks -- [https://www.geeksforgeeks.org/python-programming-language](https://www.geeksforgeeks.org/python-programming-language)</br>
+w3schools -- [https://www.w3schools.com/python](https://www.w3schools.com/python)</br>
 Exercism Python Excercises -- [https://exercism.io/my/tracks/python](https://exercism.io/my/tracks/python)
 
 
 - [x] Installing python
-- [ ] Control flows
+- [ ] Data Types
+- [ ] Variables
+- [ ] Loops & Conditional
 - [ ] Defining functions
-- [ ] Data structures
 - [ ] Modules
 - [ ] Scope & namespaces
 - [ ] Class definition
