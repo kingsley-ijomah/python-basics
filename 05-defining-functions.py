21ae12046169f95a96d1c689f9c0a0aca071d8c4
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-03-15 17:05:20 +0000

    defining functions

---
+++ 05-defining-functions.py
@@ -0,0 +1,30 @@
+'''
+https://www.w3schools.com/python/python_functions.asp
+
+
+
+Test what you learnt
+--------------------
+
+1) In python functions are defined with what key word?
+
+2) Write a function called greet, that prints "hello world!"
+
+3) What is the name of the argument passed to the function below?
+
+def my_function(fname):
+  print("my firstname is: " + fname)
+
+4) define a function called my_function that accpets arbitrary number of args
+
+5) define a function called my_function that accpets arbitrary keyword number of args
+
+6) What would be printed if you remove the "return" keyword from below?
+
+def my_function(x):
+  return 5 * x
+
+7) What is wrong with the function below?
+
+def myfunction():
+  
