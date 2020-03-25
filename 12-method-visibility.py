a5a301c2c94d7fd84b059f9ff26702e385dabdb0
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-03-25 16:01:09 +0000

    adding method visibility in python

---
+++ 12-method-visibility.py
@@ -0,0 +1,3 @@
+# https://www.geeksforgeeks.org/encapsulation-in-python/
+# https://www.tutorialsteacher.com/python/private-and-protected-access-modifiers-in-python
+# https://radek.io/2011/07/21/private-protected-and-public-in-python/
