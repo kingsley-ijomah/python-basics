0a3881e741b8d2bbd6f0a931bb67ea37cd898b3a
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-04-10 14:17:58 +0100

    complete scrable exercise

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,14 @@
+SCORES = [
+    (1, 'A, E, I, O, U, L, N, R, S, T'),
+    (2, 'D, G'),
+    (3, 'B, C, M, P'),
+    (4, 'F, H, V, W, Y'),
+    (5, 'K'),
+    (8, 'J, X'),
+    (10, 'Q, Z')
+]
 def score(word):
-    pass
+    return sum([letter_scores(letter) for letter in word.upper()])
+
+def letter_scores(letter):
+    return sum([score for score,values in SCORES if letter in values])
