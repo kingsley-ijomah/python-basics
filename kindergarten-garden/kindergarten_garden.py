7ae9285a2529932799f7fc7922f3d4e7200fad8e
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-04-12 13:08:14 +0100

    implementing solution

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -16,4 +16,26 @@ PLANTS = {"G":"Grass", "C":"Clover", "R":"Radishes", "V":"Violets"}
 
 class Garden:
     def __init__(self, diagram, students):
-        rows = diagram.split('\n')
+        self.diagram = diagram.split('\n')
+        self.students = sorted(students) or STUDENTS
+
+    def plant_rows(self):
+        res = []
+        for row in self.diagram:
+            res.append([row[i:i+2] for i in range(0,len(row),2)])
+        return res
+
+    def fetch_plants(self, student):
+        res = []
+        plant_rows = self.plant_rows()
+        for key,plant in enumerate(plant_rows):
+            res.append(plant_rows[key][self.students.index(student)])
+        return res
+
+    def plants(self, student):
+        res = []
+        for initial in ''.join(self.fetch_plants(student)):
+            res.append(PLANTS[initial])
+        return res
+    
+
