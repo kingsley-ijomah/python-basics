ea78700fedb8def749b5e5eb784faa05d8fefa02
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-04-25 16:40:05 +0100

    adding solution rational number

--- rational-numbers/rational_numbers.py
+++ rational-numbers/rational_numbers.py
@@ -1,10 +1,16 @@
 from __future__ import division
+from math import gcd
 
 
 class Rational:
     def __init__(self, numer, denom):
-        self.numer = None
-        self.denom = None
+        self.numer, self.denom  = Rational.reduce(numer, denom)
+        self.numer = -self.numer if self.denom < 0 else self.numer
+
+    @classmethod
+    def reduce(cls, numer, denom):
+        _gcd = gcd(numer, denom)
+        return (numer // _gcd, denom // _gcd)
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
