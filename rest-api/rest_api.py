3ee66e01120e868889a7a51a1a1a3776e5a4fb63
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-04-29 11:00:23 +0100

    adding get and post partial solution

--- rest-api/rest_api.py
+++ rest-api/rest_api.py
@@ -1,9 +1,33 @@
+import json
+
 class RestAPI:
     def __init__(self, database=None):
-        pass
+        self.database = database
 
     def get(self, url, payload=None):
-        pass
+        if url == '/users' and payload == None:
+            return json.dumps(self.database)
 
     def post(self, url, payload=None):
-        pass
+        if url == '/add' and payload:
+            return RestAPI.user_parser(payload)
+
+    @classmethod
+    def user_parser(cls, payload):
+        payload = json.loads(payload)
+        return json.dumps({
+            "name": payload["user"], 
+            "owes": {}, 
+            "owed_by": {}, 
+            "balance": 0.0
+        })
+
+
+database = {"users": []}
+api = RestAPI(database)
+payload = json.dumps({"user": "Adam"})
+
+print(api.get("/users",payload))
+
+
+
