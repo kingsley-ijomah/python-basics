d0224e4f40f04ddf71e39f7db53752d97504770b
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-05-03 17:39:40 +0100

    adding regex example

--- regex.py
+++ regex.py
@@ -1,7 +1,10 @@
 # regex examples
 
-# Matching Anything But a Newline
+# Matching Anything But a Newline e.g ( 123.456.abc.def )
 regex_pattern = r"\.(\d{3})|(\w{3})"
 
-# Matching Digits & Non-Digit Characters
+# Matching Digits & Non-Digit Characters e.g (06-11-2015)
 Regex_Pattern = r"\d[^\d]"
+
+# Matching Whitespace & Non-Whitespace Character ( 12 11 15 )
+Regex_Pattern = r"\d{2}\s\d{2}\s\d{2}"
