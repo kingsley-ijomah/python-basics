5206fcb839e940b728ae5cd34712314714aabcb8
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-05-04 17:25:42 +0100

    update regex

--- regex.py
+++ regex.py
@@ -8,3 +8,20 @@ Regex_Pattern = r"\d[^\d]"
 
 # Matching Whitespace & Non-Whitespace Character ( 12 11 15 )
 Regex_Pattern = r"\d{2}\s\d{2}\s\d{2}"
+
+# Matching Word & Non-Word Character ( www.hackerrank.com )
+r"\w{3}\W{1}\w{10}\W{1}\w{3}"
+
+# Matching Start & End ( 0qwer. )
+Regex_Pattern = r"^\d\w{4}\.$"
+
+# Matching Specific Characters ( 1203x. )  
+Regex_Pattern = r'^[1|2|3][1|2|0][x|s|0][3|0|A|a][x|s|u][.|,]$'
+
+# Matching Start & End ( 0qwer. )   
+r"^\d\w{4}\.$"
+
+# Excluding Specific Characters ( think? )
+Regex_Pattern = r'^[^0-9][^aeiou][^bcDF][^\s][^AEIOU][^.,]$'
+
+
