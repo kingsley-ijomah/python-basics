4d1efb662a4ae551b9d8461b3eb90662cbd40b6d
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-05-12 09:42:00 +0100

    add regex solution

--- regex.py
+++ regex.py
@@ -1,10 +1,10 @@
 # regex examples
 
 # Matching Anything But a Newline e.g ( 123.456.abc.def )
-regex_pattern = r"\.(\d{3})|(\w{3})"
+regex_pattern = r"^...\....\....\....$"
 
 # Matching Digits & Non-Digit Characters e.g (06-11-2015)
-Regex_Pattern = r"\d[^\d]"
+Regex_Pattern = r"^\d{2}[^\d]{1}\d{2}[^\d]{1}\d{4}"
 
 # Matching Whitespace & Non-Whitespace Character ( 12 11 15 )
 Regex_Pattern = r"\d{2}\s\d{2}\s\d{2}"
